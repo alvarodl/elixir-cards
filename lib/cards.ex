68590a40a25d596f038fee151721347da404c210
Álvaro Domínguez López <alvarodl20@outlook.com>
2023-02-15 19:31:09 +0100

    doc(cards): add documentation to cards module

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,4 +1,11 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -8,13 +15,39 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Determines whether a deck contains a given card
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_Size` argument indicates how many cards should
+    be in the hand
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
-    |> elem(0)
   end
 
   def save(deck, filename) do
@@ -22,10 +55,17 @@ defmodule Cards do
     File.write(filename, binary)
   end
 
+  @spec load(
+          binary
+          | maybe_improper_list(
+              binary | maybe_improper_list(any, binary | []) | char,
+              binary | []
+            )
+        ) :: any
   def load(filename) do
     case File.read(filename) do
       {:ok, binary} -> :erlang.binary_to_term(binary)
-      {:error, _reason} -> "That file does not exist"
+      {:error, :enoent} -> "That file does not exist"
     end
   end
 
@@ -33,9 +73,5 @@ defmodule Cards do
     Cards.create_deck()
     |> Cards.shuffle()
     |> Cards.deal(hand_size)
-  end
-
-  def contains?(deck, card) do
-    Enum.member?(deck, card)
   end
 end
